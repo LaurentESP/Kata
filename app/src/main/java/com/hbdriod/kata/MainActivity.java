c13949013a8a5efc5a0338f176b9ef79fdbff2a3
Laurent34 <laurent.esperance@gmail.com>
2016-09-27 16:36:19 +0200

    Add method to GUI

--- app/src/main/java/com/hbdriod/kata/MainActivity.java
+++ app/src/main/java/com/hbdriod/kata/MainActivity.java
@@ -3,6 +3,7 @@ package com.hbdriod.kata;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -17,8 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
-        TextView textView = (TextView) findViewById(R.id.text_view_0);
-        EditText editText = (EditText) findViewById(R.id.edit_text_0);
+        final TextView textView = (TextView) findViewById(R.id.text_view_0);
+        final EditText editText = (EditText) findViewById(R.id.edit_text_0);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myString = new String(editText.getText().toString());
+                textView.setText(String.valueOf(StringCalculator.add(myString)));
+            }
+        });
 
     }
 }
